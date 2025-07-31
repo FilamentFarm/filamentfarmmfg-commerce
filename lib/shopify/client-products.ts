d2c06545d98d3664b83f1bbc5cb527134d514bc4
FilamentFarm <filamentfarm@gmail.com>
2025-07-30 21:34:48 -0600

    Delete lib/shopify/client-products.ts

--- lib/shopify/client-products.ts
+++
@@ -1,7 +0,0 @@
-'use server';
-
-import { getCollectionProducts } from './index';
-
-export async function getClientProducts(collectionHandle: string) {
-  return await getCollectionProducts({ collection: collectionHandle });
-}
