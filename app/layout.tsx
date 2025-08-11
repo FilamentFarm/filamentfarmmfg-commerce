81f4808a18ca6b9c3edd4a58105cc1e25d7e3c9b
FilamentFarm <filamentfarm@gmail.com>
2025-08-10 19:30:15 -0600

    chore(ui): remove welcome toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { CartProvider } from 'components/cart/cart-context';
 import { Navbar } from 'components/layout/navbar';
-import { WelcomeToast } from 'components/welcome-toast';
+// import { WelcomeToast } from 'components/welcome-toast';
 import { GeistSans } from 'geist/font/sans';
 import { getCart } from 'lib/shopify';
 import { getClientConfig } from 'lib/get-client-config';
@@ -54,7 +54,7 @@ export default async function Layout({
           <main>
             {children}
             <Toaster closeButton />
-            <WelcomeToast />
+            {/*<WelcomeToast />*/}
           </main>
         </CartProvider>
       </body>
