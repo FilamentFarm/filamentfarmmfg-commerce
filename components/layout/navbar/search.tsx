ea5ab66e5483e56a6acf9082011f153b403f0d28
Nick <Nick@filamentfarm.net>
2025-12-02 22:49:52 +0000

    fix search

--- components/layout/navbar/search.tsx
+++ components/layout/navbar/search.tsx
@@ -1,14 +1,29 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
-import Form from 'next/form';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 
 export default function Search() {
+  const router = useRouter();
   const searchParams = useSearchParams();
 
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const val = e.currentTarget.q.value as string;
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+
+    if (val) {
+      newSearchParams.set('q', val);
+    } else {
+      newSearchParams.delete('q');
+    }
+
+    router.push(`/search?${newSearchParams.toString()}`);
+  }
+
   return (
-    <Form action="/search" className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
+    <form onSubmit={onSubmit} className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
         key={searchParams?.get('q')}
         type="text"
@@ -17,20 +32,6 @@ export default function Search() {
         autoComplete="off"
         defaultValue={searchParams?.get('q') || ''}
         className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
-      />
-      <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
-        <MagnifyingGlassIcon className="h-4" />
-      </div>
-    </Form>
-  );
-}
-
-export function SearchSkeleton() {
-  return (
-    <form className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
-      <input
-        placeholder="Search for products..."
-        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
