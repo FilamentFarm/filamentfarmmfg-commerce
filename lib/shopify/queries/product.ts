d6983c2397bc214bda2e0412ecba3439db602221
Nick <Nick@filamentfarm.net>
2025-12-02 21:33:44 +0000

    fix products

--- lib/shopify/queries/product.ts
+++ lib/shopify/queries/product.ts
@@ -10,7 +10,7 @@ export const getProductQuery = /* GraphQL */ `
 `;
 
 export const getProductsQuery = /* GraphQL */ `
-  query getProducts($sortKey: ProductSortKeys, $reverse: Boolean, $query: String, $first: Int, $after: String) {
+  query getProducts($sortKey: ProductSortKeys, $reverse: Boolean, $query: String, $first: Int = 250, $after: String) {
     products(sortKey: $sortKey, reverse: $reverse, query: $query, first: $first, after: $after) {
       edges {
         cursor
