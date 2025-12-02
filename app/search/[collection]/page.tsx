65043e419f74fc8fca3ab4c1e0922c64a434b448
Nick <Nick@filamentfarm.net>
2025-12-02 21:50:04 +0000

    fix tag display

--- app/search/[collection]/page.tsx
+++ app/search/[collection]/page.tsx
@@ -1,40 +1,40 @@
-import { getCollection, getCollectionProducts } from 'lib/shopify';
+import { getProducts } from 'lib/shopify';
 import { Metadata } from 'next';
-import { notFound } from 'next/navigation';
 
 import Grid from 'components/grid';
 import ProductGridItems from 'components/layout/product-grid-items';
 import { defaultSort, sorting } from 'lib/constants';
 
 export async function generateMetadata(props: {
-  params: Promise<{ collection: string }>;
+  params: { collection: string };
 }): Promise<Metadata> {
-  const params = await props.params;
-  const collection = await getCollection(params.collection);
-
-  if (!collection) return notFound();
+  const tag = props.params.collection;
 
   return {
-    title: collection.seo?.title || collection.title,
-    description:
-      collection.seo?.description || collection.description || `${collection.title} products`
+    title: `Products tagged with ${tag}`,
+    description: `All products tagged with ${tag}`
   };
 }
 
-export default async function CategoryPage(props: {
-  params: Promise<{ collection: string }>;
-  searchParams?: Promise<{ [key: string]: string | string[] | undefined }>;
+export default async function TagPage(props: {
+  params: { collection: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
-  const searchParams = await props.searchParams;
-  const params = await props.params;
-  const { sort } = searchParams as { [key: string]: string };
+  const searchParams = props.searchParams;
+  const tag = props.params.collection;
+  const { sort } = (searchParams || {}) as { [key: string]: string };
   const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
-  const products = await getCollectionProducts({ collection: params.collection, sortKey, reverse });
+
+  const products = await getProducts({
+    query: `tag:${tag}`,
+    sortKey,
+    reverse
+  });
 
   return (
     <section>
       {products.length === 0 ? (
-        <p className="py-3 text-lg">{`No products found in this collection`}</p>
+        <p className="py-3 text-lg">{`No products found with tag \`${tag}\'`}</p>
       ) : (
         <Grid className="grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           <ProductGridItems products={products} />
