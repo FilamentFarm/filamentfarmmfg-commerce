8422dfbb8a33079e4d655e14d426e0a72815491b
Nick <Nick@filamentfarm.net>
2025-12-02 22:42:11 +0000

    debug

--- app/[[...slug]]/page.tsx
+++ app/[[...slug]]/page.tsx
@@ -1,8 +1,8 @@
-// app/[[...slug]]/page.tsx
-
 import ClientLogoBanner from 'components/layout/client-logo-banner';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
+import { getClientConfig } from 'lib/get-client-config';
+import { notFound } from 'next/navigation';
 
 export const metadata = {
   description:
@@ -11,6 +11,12 @@ export const metadata = {
 };
 
 export default async function HomePage() {
+  const clientConfig = await getClientConfig();
+
+  if (!clientConfig) {
+    return notFound();
+  }
+
   return (
     <>
       <ClientLogoBanner />
