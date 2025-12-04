4d1f290726b9100392afdb2e8cbae9ea6cf90480
FilamentFarm <filamentfarm@gmail.com>
2025-12-03 20:54:59 -0700

    Fix product provider props

--- components/product/product-context.tsx
+++ components/product/product-context.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import React, { createContext, useContext, useMemo, useOptimistic } from 'react';
+import { Product } from 'lib/shopify/types';
 
 type ProductState = {
   [key: string]: string;
@@ -17,13 +18,42 @@ type ProductContextType = {
 
 const ProductContext = createContext<ProductContextType | undefined>(undefined);
 
-export function ProductProvider({ children }: { children: React.ReactNode }) {
+export function ProductProvider({
+  children,
+  product
+}: {
+  children: React.ReactNode;
+  product?: Product;
+}) {
   const searchParams = useSearchParams();
 
   const getInitialState = () => {
     const params: ProductState = {};
     for (const [key, value] of searchParams.entries()) {
       params[key] = value;
+    }
+
+    if (product) {
+      const lowercaseParamKeys = Object.keys(params);
+
+      product.options.forEach((option) => {
+        const optionKey = option.name.toLowerCase();
+
+        if (!lowercaseParamKeys.includes(optionKey) && option.values.length === 1) {
+          params[optionKey] = option.values[0];
+        }
+      });
+
+      const fallbackVariant =
+        product.variants.find((variant) => variant.availableForSale) || product.variants[0];
+
+      fallbackVariant?.selectedOptions.forEach((selectedOption) => {
+        const optionKey = selectedOption.name.toLowerCase();
+
+        if (!lowercaseParamKeys.includes(optionKey)) {
+          params[optionKey] = selectedOption.value;
+        }
+      });
     }
     return params;
   };
