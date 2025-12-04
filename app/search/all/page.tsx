edcc612783c02e0a8c004a8a0af2da1628419acb
Nick <Nick@filamentfarm.net>
2025-12-04 03:43:32 +0000

    debug

--- app/search/all/page.tsx
+++ app/search/all/page.tsx
@@ -10,11 +10,12 @@ export const metadata = {
   description: 'Search for products in the store.'
 };
 
-export default async function SearchPage({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
+export default async function SearchPage({ searchParams: searchParamsPromise }: { searchParams?: Promise<{ [key: string]: string | string[] | undefined }> }) {
   const clientConfig = await getClientConfig();
   if (!clientConfig) return notFound();
 
-  const { sort } = searchParams as { [key: string]: string };
+  const searchParams = await searchParamsPromise; // Await the promise
+  const { sort } = (searchParams || {}) as { [key: string]: string };
   const { sortKey, reverse } = sorting.find((item) => item.slug === sort) || defaultSort;
   const products = await getProducts({ sortKey, reverse });
 
