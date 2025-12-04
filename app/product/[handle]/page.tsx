38978d14702ae23b29829bf780316d5383c73b39
Nick <Nick@filamentfarm.net>
2025-12-04 03:38:27 +0000

    debug

--- app/product/[handle]/page.tsx
+++ app/product/[handle]/page.tsx
@@ -6,7 +6,7 @@ import { getProduct, getProductRecommendations } from 'lib/shopify';
 import { Image } from 'lib/shopify/types';
 import Footer from 'components/layout/footer';
 import { ProductDescription } from 'components/product/product-description';
-import { ProductImages } from 'components/product/product-images';
+import { Gallery } from 'components/product/gallery';
 import { ProductProvider } from 'components/product/product-context';
 import Link from 'next/link';
 import { GridTileImage } from 'components/grid/tile';
@@ -55,7 +55,7 @@ export default async function ProductPage({ params }: { params: { handle: string
         <div className="mx-auto max-w-screen-2xl px-4">
           <div className="flex flex-col rounded-lg border border-neutral-200 bg-white p-8 md:p-12 lg:flex-row lg:gap-8 dark:border-neutral-800 dark:bg-black">
             <div className="h-full w-full basis-full lg:basis-4/6">
-              <ProductImages images={product.images as Image[]} />
+              <Gallery images={product.images as Image[]} />
             </div>
             <div className="basis-full lg:basis-2/6">
               <Suspense fallback={null}>
